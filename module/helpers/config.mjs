b5711e41f4d08b0e22b38596d4712948e2e8e8a7
Dachius <53961420+Dachius@users.noreply.github.com>
2022-11-05 23:10:40 -0500

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const SWN = {};
+
+// Define constants here, such as:
+SWN.foobar = {
+  'bas': 'SWN.bas',
+  'bar': 'SWN.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ SWN.abilities = {
+  "str": "SWN.AbilityStr",
+  "dex": "SWN.AbilityDex",
+  "con": "SWN.AbilityCon",
+  "int": "SWN.AbilityInt",
+  "wis": "SWN.AbilityWis",
+  "cha": "SWN.AbilityCha"
+};
+
+SWN.abilityAbbreviations = {
+  "str": "SWN.AbilityStrAbbr",
+  "dex": "SWN.AbilityDexAbbr",
+  "con": "SWN.AbilityConAbbr",
+  "int": "SWN.AbilityIntAbbr",
+  "wis": "SWN.AbilityWisAbbr",
+  "cha": "SWN.AbilityChaAbbr"
+};
